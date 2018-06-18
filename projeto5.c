2a284505181124a1cf83f5de18a5d01a18a94418
ericGOD24 <ericobandeira@hotmail.com>
2018-06-17 23:08:34 -0300

    vai lá que ta bonito agora

--- projeto5.c
+++ projeto5.c
@@ -96,16 +96,18 @@ char menu(){
   do{
     system("clear");
     printf("\n");
-    printf("\t1 - Mostrar arvore\n\n");
-    printf("\t2 - Verificar se a arvore é cheia\n\n");
-    printf("\t3 - Procurar o numero na arvore\n\n");
-    printf("\t4 - Verificar altura da arvore\n\n");
-    printf("\t5 - Remover o numero da arvore\n\n");
-    printf("\t6 - Printar - In Order\n\n");
-    printf("\t7 - Printar - Pre Order\n\n");
-    printf("\t8 - Printar - Post Order\n\n");
-    printf("\t9 - Balancear arvore\n\n");
-    printf("\t0 - Sair\n\n");
+    printf("==========================================");
+    printf("||\t1 - Mostrar arvore                  ||\n");
+    printf("||\t2 - Verificar se a arvore é cheia   ||\n");
+    printf("||\t3 - Procurar o numero na arvore     ||\n");
+    printf("||\t4 - Verificar altura da arvore      ||\n");
+    printf("||\t5 - Remover o numero da arvore      ||\n");
+    printf("||\t6 - Printar - In Order              ||\n");
+    printf("||\t7 - Printar - Pre Order             ||\n");
+    printf("||\t8 - Printar - Post Order            ||\n");
+    printf("||\t9 - Balancear arvore                ||\n");
+    printf("||\t0 - Sair                            ||\n");
+    printf("==========================================");
     scanf("%c",&select);
     limpaBuff();
   } while(select < '0' || select > '9');
